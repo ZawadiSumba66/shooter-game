16b5bfb812704f6177c5284447e2be2344265a04
ZawadiSumba66 <rosesumba98@gmail.com>
2021-05-09 16:51:44 +0300

    Add the setScore API method

--- src/api.js
+++ src/api.js
@@ -1,6 +1,23 @@
 let url ='https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Zl4d7IVkemOTTVg2fUdz/scores/'
-const postScores = async() =>{
+const getScores = async() =>{
     const response = fetch(url)
     const data = await response.json();
     return data
 }
+
+const setScores = async()=>{
+    const playerData = { user: player, score: scores };
+    try {
+    const response = fetch(url, {
+        method: "POST",
+        headers: {'Content-Type':'application/x-www-form-urlencoded'},
+        body: JSON.stringify(playerData)
+      })
+      const data = await response.json()
+      return data
+    }catch(error){
+        return error
+    }
+}
+
+export {setScores,getScores}
