148026225882a4586669a3ea54ede9ab1dbf6470
ZawadiSumba66 <rosesumba98@gmail.com>
2021-05-10 22:39:42 +0300

    Fix linters errors

--- src/js/SceneGameOver.js
+++ src/js/SceneGameOver.js
@@ -47,7 +47,7 @@ export default class SceneGameOver extends Phaser.Scene {
 
 
     this.backgrounds = [];
-    for (let i = 0; i < 5; i++) { // create five scrolling backgrounds
+    for (let i = 0; i < 5; i += 1) { // create five scrolling backgrounds
       const bg = new ScrollingBackground(this, 'sprBg0', i * 10);
       this.backgrounds.push(bg);
     }
